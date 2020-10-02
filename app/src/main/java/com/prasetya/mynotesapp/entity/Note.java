0ec9bc1563e20f31cdd6b25dc59e94f83c25e3a9
prass <sndr1722@gmail.com>
2020-10-02 21:45:26 +0700

    init commit

---
+++ app/src/main/java/com/prasetya/mynotesapp/entity/Note.java
@@ -0,0 +1,82 @@
+package com.prasetya.mynotesapp.entity;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Note implements Parcelable {
+    public static final Creator<Note> CREATOR = new Creator<Note>() {
+        @Override
+        public Note createFromParcel(Parcel in) {
+            return new Note(in);
+        }
+
+        @Override
+        public Note[] newArray(int size) {
+            return new Note[size];
+        }
+    };
+    private int id;
+    private String title;
+    private String description;
+    private String date;
+
+    public Note(Parcel in) {
+        id = in.readInt();
+        title = in.readString();
+        description = in.readString();
+        date = in.readString();
+    }
+
+    public Note(int id, String title, String description, String date) {
+    }
+
+    public Note() {
+
+    }
+
+    //getter and setter
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(id);
+        dest.writeString(title);
+        dest.writeString(description);
+        dest.writeString(date);
+    }
+}
